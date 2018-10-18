634d77b4ae2116bca6d8ca370bcefd108ea6cd75
repain paul <prepain@(none)>
2018-10-18 11:39:04 +0200

    manipulation de tableaux 2D dynamiques

--- tp_sysex/tp_tabdynam.c
+++ tp_sysex/tp_tabdynam.c
@@ -8,6 +8,8 @@
 /*         FONCTIONS         */
 /*****************************/
 
+// TABLEAU 1D
+void test_array_1D();
 int input_array_size();
 void input_array_size_v2(int * size);
 int * alloc_1D_array(int size);
@@ -16,24 +18,19 @@ void fill_array(int * array, int size);
 void display_array(int * array, int size);
 void free_array(int * array);
 
-
-
+// TABLEAU 2D
+void test_array_2D();
+void input_array_dimension(int dimension[2]);
+int ** alloc_2D_array(int dimension[2]);
+//void alloc_2D_array_v2(int ** array, int dimension[2]);
+void fill_2D_array(int ** array, int dimension[2]);
+void display_2D_array(int ** array, int dimension[2]);
+void free_2D_array(int ** array, int dimension[2]);
 
 int main(int argc, char* argv[])
 {
-    /*
-    int size = input_array_size();
-    printf("TAILLE : %d\n", size);
-    int * tab = alloc_1D_array(size);
-    */
-    int size;
-    input_array_size_v2(&size);
-    printf("TAILLE : %d\n", size);
-    int * tab = NULL;
-    alloc_1D_array_v2(&tab, size);
-    fill_array(tab, size);
-    display_array(tab, size);
-    free_array(tab);
+    // test_array_1D();
+    test_array_2D();
     return 0;
 }
 
@@ -44,6 +41,45 @@ int main(int argc, char* argv[])
 /*            DES            */
 /*         FONCTIONS         */
 /*****************************/
+
+void test_array_1D()
+{
+    /*
+    int size = input_array_size();
+    printf("TAILLE : %d\n", size);
+    int * tab = alloc_1D_array(size);
+    */
+    int size;
+    int * tab = NULL;
+    input_array_size_v2(&size);
+    printf("TAILLE : %d\n", size);
+    alloc_1D_array_v2(&tab, size);
+    fill_array(tab, size);
+    display_array(tab, size);
+    free_array(tab);
+}
+
+void test_array_2D()
+{
+    int dimension[2];
+    int ** array = NULL;
+    input_array_dimension(dimension);
+    printf("ROW : %d\tCOLUMN : %d\n", dimension[0], dimension[1]);
+    array = alloc_2D_array(dimension);
+    fill_2D_array(array, dimension);
+    display_2D_array(array, dimension);
+    free_2D_array(array, dimension);
+}
+
+
+/*****************************/
+/*         FONCTIONS         */
+/*          TABLEAU          */
+/*             1D            */
+/*****************************/
+
+
+
 
 /*****************************/
 /* input_array_size          */
@@ -151,5 +187,61 @@ void free_array(int * array)
     free(array);
 }
 
+/*****************************/
+/*         FONCTIONS         */
+/*          TABLEAU          */
+/*             2D            */
+/*****************************/
 
 
+void input_array_dimension(int dimension[2])
+{
+    // LIGNES
+    puts("Entrez le nombre de lignes de votre tableau 2D");
+    input_array_size_v2(&dimension[0]);
+    // COLONNES
+    puts("Entrez le nombre de colonnes de votre tableau 2D");
+    input_array_size_v2(&dimension[1]);
+}
+
+
+int ** alloc_2D_array(int dimension[2])
+{
+    int ligne;
+    int ** array = (int **) malloc(dimension[0] * sizeof(int *));
+    for (ligne = 0; ligne < dimension[0]; ligne++)
+    {
+        array[ligne] = (int *) malloc(dimension[1] * sizeof(int));
+    }
+    return array;
+}
+
+void fill_2D_array(int ** array, int dimension[2])
+{
+    int ligne;
+    for (ligne = 0; ligne < dimension[0]; ligne++)
+    {
+        fill_array(array[ligne], dimension[1]);
+    }
+}
+
+void display_2D_array(int ** array, int dimension[2])
+{
+    int ligne;
+    for (ligne = 0; ligne < dimension[0]; ligne++)
+    {
+        printf("ROW %d : ", ligne);
+        display_array(array[ligne], dimension[1]);
+    }
+}
+
+
+
+void free_2D_array(int ** array, int dimension[2])
+{
+    int ligne;
+    for (ligne = 0; ligne < dimension[0]; ligne++)
+    {
+        free_array(array[ligne]);
+    }
+}
